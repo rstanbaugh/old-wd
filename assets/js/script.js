5b77deea5dca180b3049b126c30e829a9461ad3d
rstanbaugh <rich_bootcamp@icloud.com>
2022-05-09 18:10:12 -0400

    basic HTML starter

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var cities = [];
+
+
+var getWeather = function(city){
+  var apiKey = "1c357a3191ed3689b3d98c44e3cbb7a6";
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=1c357a3191ed3689b3d98c44e3cbb7a6"
+
+  // make a request to the api
+  fetch(apiUrl).then (response => {
+    //  check if api returned any weather
+    if(response.ok){
+      response.json()
+        .then (data => console.log(data));
+    } else {
+      alert("Error: City Not Found");
+    }
+  });
+};
+
+
+getWeather("livonia");

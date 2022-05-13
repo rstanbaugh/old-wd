2cbefb7d113d2929ec7276cf445c1e48663bd036
rstanbaugh <rich_bootcamp@icloud.com>
2022-05-13 08:27:11 -0400

    added uvi

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,7 +35,11 @@ var geoCodeCity = function (location) {
           // console.log(selectedCity.label);
           // console.log(selectedCity);
 
-          getWeather(selectedCity.lat, selectedCity.lon)
+          getCurrentWeather(selectedCity.lat, selectedCity.lon);
+          getUvi(selectedCity.lat, selectedCity.lon);
+          getForecast(selectedCity.lat, selectedCity.lon);
+          displayWeather(data);
+
         });
     } else {
       alert("Error: City Not Found");
@@ -45,13 +49,14 @@ var geoCodeCity = function (location) {
 }
 
 var displayWeather = function(data){
-  $("#selected-city").text(selectedCity.label)
-  $("#current-temp").text("Temp: "+currentWeather.temp+" (feels like "+currentWeather.feelsLike+")");
-  $("#current-wind").text("Wind: "+currentWeather.wind+" from "+currentWeather.windDeg.toFixed(0)+" deg, Gusting "+currentWeather.windGust.toFixed(1)+" mph");
-  $("#current-humidity").text("Humidity: "+currentWeather.humidity+"%");
+  $("#selected-city").html(selectedCity.label)
+  $("#current-temp").html("<b>Temp:</b> "+currentWeather.temp+" (feels like "+currentWeather.feelsLike+")");
+  $("#current-wind").html("<b>Wind:</b> "+currentWeather.wind+" from "+currentWeather.windDeg+" deg, Gusting "+currentWeather.windGust+" mph");
+  $("#current-humidity").html("<b>Humidity:</b> "+currentWeather.humidity+"%");
+  $("#current-uvi").html("<b>UV Index:</b> "+currentWeather.uvi);
   
 };
-var getWeather = function(lat, lon){
+var getCurrentWeather = function(lat, lon){
 
   // var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+apiKey;
 
@@ -64,31 +69,31 @@ var getWeather = function(lat, lon){
         .then (data => {
           // write to object and handles data errors with "-"
           if (data.main.hasOwnProperty("temp")){
-            currentWeather.temp = data.main.temp;
+            currentWeather.temp = data.main.temp.toFixed(0);
           } else {currentWeather.temp = "n/a";}
 
           if(data.main.hasOwnProperty("feels_like")) {
-            currentWeather.feelsLike = data.main.feels_like;
+            currentWeather.feelsLike = data.main.feels_like.toFixed(0);
           } else {currentWeather.feelsLike = "n/a";}
 
           if(data.main.hasOwnProperty("humidity")) {
-            currentWeather.humidity = data.main.humidity;
+            currentWeather.humidity = data.main.humidity.toFixed(0);
           } else {currentWeather.humidity = "n/a";}
           
           if(data.main.hasOwnProperty("pressure")) {
-            currentWeather.pressure = data.main.pressure;
+            currentWeather.pressure = data.main.pressure.toFixed(1);
           } else {currentWeather.pressure = "n/a";}
 
           if(data.wind.hasOwnProperty("deg")) {
-            currentWeather.windDeg = data.wind.deg;
+            currentWeather.windDeg = data.wind.deg.toFixed(0);
           } else {currentWeather.windDeg = "n/a";}
 
           if(data.wind.hasOwnProperty("speed")) {
-            currentWeather.windSpeed = data.wind.speed;
+            currentWeather.windSpeed = data.wind.speed.toFixed(0);
           } else {currentWeather.windSpeed = "n/a";}
 
           if(data.wind.hasOwnProperty("gust")) {
-            currentWeather.windGust = data.wind.gust;
+            currentWeather.windGust = data.wind.gust.toFixed(0);
           } else {currentWeather.windGust = "n/a";}
 
           displayWeather(data)
@@ -101,8 +106,51 @@ var getWeather = function(lat, lon){
   });
 };
 
+var getUvi = function(lat, lon){
 
+  // var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid="+apiKey;
 
+  var apiUrl = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKeyOpenWeather}`
+  // make a request to the api
+  
+  fetch(apiUrl).then (response => {
+    //  check if api returned any weather
+    if(response.ok){
+      response.json()
+        .then (data => {
+          // write to object and handles data errors with "-"
+          if (data.hasOwnProperty("value")){
+            currentWeather.uvi = data.value.toFixed(1);
+          } else {currentWeather.uvi = "n/a";}
+        });
+    } else {
+      alert("Error: City Not Found");
+    }
+  });
+};
+
+var getForecast = function(lat, lon){
+
+  var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKeyOpenWeather}`
+debugger
+  var apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKeyOpenWeather}`
+  // make a request to the api
+  fetch(apiUrl).then (response => {
+    //  check if api returned any weather
+    if(response.ok){
+      response.json()
+        .then (data => {
+          debugger
+          // write to object and handles data errors with "-"
+          if (data.hasOwnProperty("value")){
+            currentWeather.uvi = data.value.toFixed(1);
+          } else {currentWeather.uvi = "n/a";}
+        });
+    } else {
+      alert("Error: City Not Found");
+    }
+  });
+};
 geoCodeCity("livonia, MI");
 // Livonia, MI = 4999837
 // getWeather("4999837");
